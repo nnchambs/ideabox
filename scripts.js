21fca0b97dbeca36bd0f27da04573feb736fa86b
Nick <nicknchambers@gmail.com>
2016-09-07 21:16:59 -0600

    Created generateNewIdea function that captures value of title and body input fields

--- scripts.js
+++ scripts.js
@@ -8,17 +8,33 @@ function getIdeaBodyInput () {
   var ideaBodyInput = $('#idea-body-input').val();
 };
 
-
 //an idea object has a unique ID, title, body, and a quality.
 function Idea(title, body) {
   this.title = title;
   this.body = body;
+  this.quality = "swill";
+
 };
 
-var myIdea = new Idea ("bahah", "alalala", quality);
+
+function generateNewIdea() {
+  var newIdea = new Idea();
+  newIdea.title = $('#idea-title-input').val();
+  newIdea.body = $('#idea-body-input').val();
+};
+
+
+//function to generate new Idea
+var myIdea = new Idea ("bahah", "alalala");
 
 // event listener for save button to trigger getInput functions
 $('.container').on('click', '.save-button', function(){
-  getIdeaTitleInput();
-  getIdeaBodyInput();
+  generateNewIdea();
 });
+
+
+
+// this.uniqueId = function() {
+//    var date = Date.now().toString();
+//    return date;
+// }
