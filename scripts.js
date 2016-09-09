7436956a7766f8b7381923952d3afc74c5d33925
Nick <nicknchambers@gmail.com>
2016-09-09 13:14:46 -0600

    Added appendNewIdea function

--- scripts.js
+++ scripts.js
@@ -24,9 +24,17 @@ function generateNewIdea() {
 };
 
 
+//function to append new idea to the idea box DOM
+
+function appendNewIdea() {
+  var titleInput = $('#idea-title-input').val();
+  var bodyInput = $('#idea-body-input').val();
+  $('.results-container').append(`<h1 class="idea-output">${titleInput}<button type="image" class="delete"></button></h1> <p class="idea-body-output">${bodyInput}</p> <p class="buttons"> <button type="image" class="upVote"></button> <button type="image" class="downVote"></button> <span class="idea-quality">quality</span>:<span class="idea-quality-rank">swill</span>`);
+};
 
 
 // event listener for save button to trigger getInput functions
 $('.container').on('click', '.save-button', function(){
   generateNewIdea();
+  appendNewIdea();
 });
