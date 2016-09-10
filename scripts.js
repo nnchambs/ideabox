2ff515a105e3525ae9f183fb9353598bd7e15f64
Chaz-Mykl <chaz.mykl@gmail.com>
2016-09-10 11:53:56 -0600

    Created function 'grabStorageAndRender' which stores input in local storage and renders the input onto the page

--- scripts.js
+++ scripts.js
@@ -1,51 +1,61 @@
 //an idea object has a unique ID, title, body, and a quality.
-function Idea(title, body) {
+function Idea(title, body, quality, id) {
   this.title = title;
   this.body = body;
-  this.quality = "swill";
-  this.uniqueID = Date.now();
+  this.quality = quality || "swill";
+  this.uniqueID = id || Date.now();
 };
 
 var ideaBox = {
   ideas : [],
   // add :
-  // remove :
+  remove : function uniqueID(id) {
+          var uniqueID = parseInt(id);
+          this.ideas = this.ideas.filter(function(Idea) {
+            return Idea.uniqueID !== uniqueID;
+            this.store
+          });
+  },
 
   store : function generateNewIdea() {
-          var newIdea = new Idea();
-          newIdea.title = $('#idea-title-input').val();
-          newIdea.body = $('#idea-body-input').val();
-          if (localStorage.ideas) {
-            var parsedLocalStorageIdeas = JSON.parse(localStorage.ideas);
-          }
-          else {
-            var parsedLocalStorageIdeas = [];
-          }
-          parsedLocalStorageIdeas.push(newIdea);
-          var stringifiedLocalStorageIdeas = JSON.stringify(parsedLocalStorageIdeas);
-          localStorage.setItem('ideas', stringifiedLocalStorageIdeas);
+          var title = $('#idea-title-input').val();
+          var body = $('#idea-body-input').val();
+          var newIdea = new Idea(title, body);
+          this.ideas.push(newIdea);
+          var stringifiedIdeas = JSON.stringify(this.ideas);
+          localStorage.setItem('ideas', stringifiedIdeas);
+          render(newIdea);
+          // storeIdeas();
         }
   // retrieve :
-  // render :
   // find :
 
 };
+grabStorageAndRender()
 
+function grabStorageAndRender() {
+  ideaBox.ideas = JSON.parse(localStorage.ideas) || [];
+  for(var i = 0; i < ideaBox.ideas.length; i++) {
+    var object = ideaBox.ideas[i];
+    var idea = new Idea(object.title, object.body, object.quality, object.id)
+    ideaBox.ideas[i] = idea;
+    render(idea)
+  }
+}
 
 //function to generate new idea and set it to local storage
 
 
 //function to append new idea to the idea box DOM
 
-function appendNewIdea() {
-  var titleInput = $('#idea-title-input').val();
-  var bodyInput = $('#idea-body-input').val();
-  $('.results-container').append(`<h1 class="idea-output">${titleInput}<button type="image" class="delete"></button></h1> <p class="idea-body-output">${bodyInput}</p> <p class="buttons"> <button type="image" class="upVote"></button> <button type="image" class="downVote"></button> <span class="idea-quality">quality</span>:<span class="idea-quality-rank">swill</span>`);
+function render(idea) {
+  // var titleInput = $('#idea-title-input').val();
+  // var bodyInput = $('#idea-body-input').val();
+  $('.results-container').append(`<section><h1 class="idea-output">${idea.title}<button type="image" class="delete"></button></h1> <p class="idea-body-output">${idea.body}</p> <p class="buttons"> <button type="image" class="upVote"></button> <button type="image" class="downVote"></button> <span class="idea-quality">quality</span>:<span class="idea-quality-rank">${idea.quality}</span></section>`);
 };
 
 
 // event listener for save button to trigger getInput functions
 $('.container').on('click', '.save-button', function(){
   ideaBox.store();
-  appendNewIdea();
 });
